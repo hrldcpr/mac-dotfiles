10445271da54fae3c2b9f26732807ad9b72230c4
Harold Cooper <hrldcpr@gmail.com>
2015-05-28 11:33:08 -0400

    show trailing whitespace in emacs, and delete it on save

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,12 @@
 ; http://www.emacswiki.org/emacs/NoTabs
 (setq-default indent-tabs-mode nil)
 
+; http://emacswiki.org/emacs/ShowWhiteSpace
+(setq-default show-trailing-whitespace 't)
+
+; http://emacswiki.org/emacs/DeletingWhitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
 (let ((cask (if (string-equal system-type "darwin")
 		"/usr/local/share/emacs/site-lisp/cask.el"
 	      "~/.cask/cask.el")))
