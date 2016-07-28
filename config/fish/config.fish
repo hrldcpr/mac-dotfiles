b5781ad5977185195d43cd205252309ae50beff6
Harold Cooper <hrldcpr@gmail.com>
2016-07-28 13:28:05 -0400

    set EDITOR, alias ipython, use default docker machine

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,9 @@
-set -x PATH $HOME/.cask/bin $PATH
+set -x EDITOR emacs
+
+set -x PATH $PATH /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/
+
+alias python ipython
+
+eval (docker-machine env default)
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
