00e05b9620c5e44e63cbbc50c31e7c73540e3263
Harold Cooper <hrldcpr@gmail.com>
2016-09-11 11:01:53 -0400

    sometimes you need python 2 😭

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,8 +2,6 @@ set -x EDITOR emacs
 
 set -x PATH $PATH /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/
 
-alias python ipython
-
 eval (docker-machine env default)
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
