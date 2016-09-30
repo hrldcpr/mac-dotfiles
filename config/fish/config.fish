dd2625dca0403920951512ee92957e683ded07ad
Harold Cooper <hrldcpr@gmail.com>
2016-09-30 16:11:07 -0400

    stop using docker machine, use docker mac

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,4 @@ set -x EDITOR emacs
 
 set -x PATH $PATH /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/
 
-eval (docker-machine env default)
-
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
