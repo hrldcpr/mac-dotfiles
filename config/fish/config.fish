ce4146e1c741e49a4f07726d2482276c70008b0b
Harold Cooper <hrldcpr@gmail.com>
2016-10-20 11:36:32 -0400

    don't have trailing slashes in PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 set -x EDITOR emacs
 
-set -x PATH $PATH /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin/
+set -x PATH $PATH /Applications/CoqIDE_8.4pl5.app/Contents/Resources/bin
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
