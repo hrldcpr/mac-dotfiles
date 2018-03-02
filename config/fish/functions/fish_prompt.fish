c980879bcf984d6ee8f7d765eef3c811fdc72691
Harold Cooper <hcooper@google.com>
2018-03-02 16:41:25 -0500

    remove unused stuff

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -10,18 +10,6 @@ function fish_prompt --description 'Write out the prompt'
     if not set -q -g __fish_classic_git_functions_defined
         set -g __fish_classic_git_functions_defined
 
-        function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-            if status --is-interactive
-                commandline -f repaint ^/dev/null
-            end
-        end
-
-        function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-            if status --is-interactive
-                commandline -f repaint ^/dev/null
-            end
-        end
-
         function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
             if status --is-interactive
                 commandline -f repaint ^/dev/null
@@ -36,10 +24,6 @@ function fish_prompt --description 'Write out the prompt'
 
         # initialize our new variables
         if not set -q __fish_classic_git_prompt_initialized
-            set -qU fish_color_user
-            or set -U fish_color_user -o green
-            set -qU fish_color_host
-            or set -U fish_color_host -o cyan
             set -qU fish_color_status
             or set -U fish_color_status red
             set -U __fish_classic_git_prompt_initialized
