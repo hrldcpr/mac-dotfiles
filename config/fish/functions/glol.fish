465227fd546d6006ee7c39e95eba612acba3a9d7
Harold Cooper <hrldcpr@gmail.com>
2018-03-26 17:56:23 -0400

    git log --color is not necessary

--- config/fish/functions/glol.fish
+++ config/fish/functions/glol.fish
@@ -1,4 +1,4 @@
 function glol
-	 git log --all --color --graph --pretty=format:\
+	 git log --all --graph --pretty=format:\
 '%C(red)%h%C(reset)%x09%C(yellow)%d%C(reset) %s %C(cyan)(%cr) %C(blue)[%aN]%C(reset)'
 end
