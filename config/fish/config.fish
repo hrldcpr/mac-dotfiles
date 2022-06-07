1c5dea2144b56acd5a8b2cb17c4ad68dda3d811c
Harold Cooper <harold@x.st>
2022-06-07 09:42:08 -0400

    fish pyenv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,6 @@
 status --is-interactive; and source (rbenv init -|psub)
 
+pyenv init - | source
+
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
